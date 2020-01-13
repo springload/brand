c0c3ca734958c9d5d19a62e8128fbad87d64e213
Mitch Ryan <mitch.ryan@outlook.co.nz>
2020-01-13 15:36:04 +1300

    Added task to generate React components for all icons

---
+++ dist/logos/index.js
@@ -0,0 +1,4 @@
+export { default as SpringloadLogoBlackOnLight } from './SpringloadLogoBlackOnLight'
+export { default as SpringloadLogoColorOnDark } from './SpringloadLogoColorOnDark'
+export { default as SpringloadLogoColorOnLight } from './SpringloadLogoColorOnLight'
+export { default as SpringloadLogoWhiteOnDark } from './SpringloadLogoWhiteOnDark'
